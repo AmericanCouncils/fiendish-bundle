0060db2ec67ed619cb59a967e325023eebd3cc35
David Simon <dsimon@americancouncils.org>
2013-01-24 09:29:39 -0500

    Starting to use sqlite for testing

--- Tests/Entity/ProcessTest.php
+++ Tests/Entity/ProcessTest.php
@@ -3,12 +3,16 @@
 namespace DavidMikeSimon\FiendishBundle\Tests\Entity;
 
 use DavidMikeSimon\FiendishBundle\Tests\FiendishTestCase;
+use DavidMikeSimon\FiendishBundle\Entity\Process;
 
 class ProcessTest extends FiendishTestCase
 {
     public function testSimple()
     {
         $container = $this->getContainer();
-        $this->assertEquals(3, 4);
+        $em = $container->get('doctrine')->getEntityManager();
+        $proc = new Process('fiendish_test', 'test_daemon', 'Test/Foo', []);
+        $em->persist($proc);
+        $em->flush();
     }
 }
