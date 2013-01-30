2e30589847911543316893d5c99c164964c25a3e
David Simon <dsimon@americancouncils.org>
2013-01-30 11:25:29 -0500

    Improved Sami setup

--- doc/config/SamiConfig.php
+++ doc/config/SamiConfig.php
@@ -1,6 +1,38 @@
 <?php
 
-return new Sami\Sami(new ArrayIterator([
-    __DIR__ . "/../../Daemon",
-    __DIR__ . "/../../Entity",
-]));
+use Sami\Sami;
+use Sami\Parser\Filter\SymfonyFilter;
+use Sami\Reflection\ClassReflection;
+use Sami\Reflection\MethodReflection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name("*.php")
+    ->exclude('Tests')
+    ->exclude('vendor')
+    ->exclude('doc')
+    ->in(__DIR__ . "/../../");
+
+class DescriptionFilter extends SymfonyFilter
+{
+    public function acceptClass(ClassReflection $class)
+    {
+        print("POINT Y\n");
+        return parent::acceptClass($class) &&
+            strlen($class->getDocBlock()->getShortDesc()) > 0;
+    }
+
+    public function acceptMethod(MethodReflection $method)
+    {
+        print("POINT X\n");
+        return parent::acceptMethod($method) &&
+            strlen($method->getDocBlock()->getShortDesc()) > 0;
+    }
+}
+
+return new Sami($iterator, [
+    'build_dir' => __DIR__ . "/../build",
+    'cache_dir' => __DIR__ . "/../cache",
+    'filter' => new SymfonyFilter()
+]);
