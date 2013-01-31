d945349f10114d2ee42ee3c3acbc60d4c87062af
David Simon <dsimon@americancouncils.org>
2013-01-31 14:29:23 -0500

    Updated docs

--- search/functions_72.js
+++ search/functions_72.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]],
   ['run',['run',['../class_daemon_1_1_base_daemon.html#a7b4285bac607114391cc45235472eb8b',1,'Daemon\BaseDaemon\run()'],['../class_daemon_1_1_master_daemon.html#a0f51efccb5f85b1dd73a6d45cc6bad45',1,'Daemon\MasterDaemon\run()']]]
 ];
